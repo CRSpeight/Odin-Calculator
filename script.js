dd6e1dd54db1a214da181a4b401e89063af3d511
Chris Speight <paradisio15@gmail.com>
2020-07-21 21:49:35 -0500

    basic calculator functionality added, supports pemdas, lots of error edge cases currently

--- script.js
+++ script.js
@@ -15,18 +15,20 @@ function divide(num1, num2) {
 }
 
 function operate(num1, operator, num2) {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
   switch (operator) {
     case "+":
-      console.log(add(num1, num2));
+      return add(num1, num2);
       break;
     case "-":
-      console.log(subtract(num1, num2));
+      return subtract(num1, num2);
       break;
-    case "*":
-      console.log(multiply(num1, num2));
+    case "x":
+      return multiply(num1, num2);
       break;
     case "/":
-      console.log(divide(num1, num2));
+      return divide(num1, num2);
       break;
     default:
       console.log("Derp");
@@ -50,6 +52,34 @@ function clearMessage() {
   message.textContent = currMessage;
 }
 
+function messageParser() {
+  // prettier-ignore
+  const seperators = [{reg:/\+/g,val:"+"},{reg:/\-/g,val:"-"},{reg:/\x/g,val:"x"},{reg:/\//g,val:"/"}];
+  let newStr = currMessage.slice(0);
+  seperators.forEach(
+    (element) => (newStr = newStr.replace(element.reg, "," + element.val + ","))
+  );
+  return newStr.split(",");
+}
+
+function equals() {
+  const seperators = ["x", "/", "+", "-"];
+  let parsedQuery = messageParser();
+  seperators.forEach(function (s) {
+    while (parsedQuery.indexOf(s) > -1) {
+      let operatorPos = parsedQuery.indexOf(s);
+      let subQuery = parsedQuery.splice(operatorPos - 1, 3);
+      parsedQuery.splice(
+        operatorPos - 1,
+        0,
+        operate(subQuery[0], subQuery[1], subQuery[2])
+      );
+    }
+  });
+  clearMessage();
+  messageUpdater(parsedQuery);
+}
+
 const calcBody = document.querySelector("#calcBody");
 
 const buttons = [
@@ -66,7 +96,7 @@ const buttons = [
   { displayName: "C", cssName: "clear", type: "clear" },
   { displayName: "+", cssName: "add", type: "function" },
   { displayName: "-", cssName: "subtract", type: "function" },
-  { displayName: "X", cssName: "multiply", type: "function" },
+  { displayName: "x", cssName: "multiply", type: "function" },
   { displayName: "/", cssName: "divide", type: "function" },
   { displayName: "=", cssName: "equal", type: "operator" },
 ];
@@ -77,11 +107,15 @@ buttons.forEach(function (button) {
   btn.classList.add(button.type);
   btn.style.gridArea = button.cssName;
   switch (button.type) {
+    case "function":
     case "digit":
       btn.addEventListener("click", (e) => messageUpdater(button.displayName));
       break;
     case "clear":
-      btn.addEventListener("click", (e) => clearMessage());
+      btn.addEventListener("click", () => clearMessage());
+      break;
+    case "operator":
+      btn.addEventListener("click", () => equals());
       break;
     default:
       break;
