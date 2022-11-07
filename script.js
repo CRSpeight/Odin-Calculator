f35bbed2117807978e2114b97735b871ba4810cf
Paradisio <paradisio15@gmail.com>
2022-11-07 13:25:15 -0600

    step 2 requirements

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(type, num1, num2) {
+  switch (type) {
+    case "add":
+      return add(num1, num2);
+      break;
+    case "subtract":
+      return subtract(num1, num2);
+      break;
+    case "multiply":
+      return multiply(num1, num2);
+      break;
+    case "divide":
+      return divide(num1, num2);
+      break;
+  }
+}
